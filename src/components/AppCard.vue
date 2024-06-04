d7f6a617da554a309b0c0e47c7e3c60fcbe9d8b3
Samudra-Ekanayake <samudra2724@gmail.com>
2024-06-04 14:57:45 +0200

    card fatte

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,33 +1,38 @@
-
-  <script>
-  export default {
+<script>
+export default {
     name: 'AppCard',
 
     data() {
         return {
 
             cards: [
-                { title: 'Languages',
-                    img: '../assets/h5-custom-icon-1.png'
+                {
+                    title: 'Languages',
+                    img: 'src/assets/h5-custom-icon-1.png'
                 },
-                { title: 'Software',
-                    img: '../assets/h5-custom-icon-2.png'
+                {
+                    title: 'Software',
+                    img: 'src/assets/h5-custom-icon-2.png'
                 },
-                { title: 'Buisness',
-                    img: '../assets/h5-custom-icon-3.png'
+                {
+                    title: 'Buisness',
+                    img: 'src/assets/h5-custom-icon-3.png'
                 },
-                { title: 'Chemistry',
-                    img: '../assets/h5-custom-icon-4.png'
+                {
+                    title: 'Chemistry',
+                    img: 'src/assets/h5-custom-icon-4.png'
                 },
-                { title: 'Science',
-                    img: '../assets/h5-custom-icon-5.png'
+                {
+                    title: 'Science',
+                    img: 'src/assets/h5-custom-icon-5.png'
                 },
-                { title: 'DIY&Craft',
-                    : '../assets/h5-custom-icon-6.png'
+                {
+                    title: 'DIY&Craft',
+                    img: 'src/assets/h5-custom-icon-6.png'
                 },
 
             ]
-            
+
         }
     },
 
@@ -42,32 +47,37 @@
 
 }
 
-  </script>
+</script>
 
 <template>
 
     <div class="container-fluid">
-        <div class="row">
-            <div v-for="element in cards" class="col-2 st_square" 
-            :style="{backgroundimage:element.img}">
+        <div class="row d-flex justify-content-center">
+            <div v-for="element in cards" class="col-2 st_square d-flex flex-column align-items-center pt-4">
+                <div class="st_card">
+                    <img :src="element.img" alt="">
+                </div>
+
                 <div>
-                    {{ element.title }}
+                    <h3>{{ element.title }}</h3>
                 </div>
             </div>
         </div>
     </div>
 
-    
+
 
 </template>
-  
-  <style scoped>
-  .st_square {
+
+<style scoped>
+.st_square {
     margin: 1rem;
     height: 12rem;
     width: 12rem;
-  }
-  
- 
+    background-color: bisque;
+}
 
-  </style>
+.st_card {
+    margin-bottom: 2rem;
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -41,12 +41,16 @@ export default {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 bg-info">
-                <div class="row">
-                    <div class="col-3 bg-warning">
-                        Logo
+                <div class="row mt-4">
+                    <div class="col-3 bg-warning st_logo">
+                        <img src="../assets/logo-light.png" alt="">
                     </div>
-                    <div class="col-7 bg-success">
-                        link
+                    <div class="col-7 bg-success d-flex justify-content-around">
+                        <ul class="d-flex">
+                            <li class="mx-4" v-for="element in navList">
+                                <a href="">{{ element }}</a>
+                            </li>
+                        </ul>
                     </div>
                     <div class="col-2 bg-primary">
                         icons
@@ -73,5 +77,12 @@ export default {
 .st_containerMain {
     height: 40rem;
     background-color: brown;
+    margin-bottom: 4rem;
+
+}
+
+.st_logo img {
+    margin-left: 4rem;
+    height: 2rem;
 }
 </style>
